806d65726dbc9f55dde3713a9000c807ffa7fc04
muninnhugin <muninnhugin@gmail.com>
2023-02-24 23:13:15 -0800

    add more helper functions to set memory blocks

--- include/helpers.h
+++ include/helpers.h
@@ -2,6 +2,7 @@
 #define HELPERS_H
 #include <stdlib.h>
 #include <stdint.h>
+#include "icsmm.h"
 
 // memory constants in bytes
 #define HEADER_SIZE 8
@@ -10,15 +11,18 @@
 #define EPILOGUE_SIZE HEADER_SIZE
 #define PAYLOAD_UNIT 16
 #define MIN_BLOCK_SIZE 32
+#define ALLOCATED 1
+#define NOT_ALLOCATED 0
 
-int first_request = 1;
-
-unsigned int calculate_ceiling(unsigned int a, unsigned int b);
+unsigned int ceiling(unsigned int a, unsigned int b);
 void get_total_block_size(unsigned int requested_payload, unsigned int* block_size, unsigned int* padding);
 void set_epilogue(void* epilogue_address);
 void set_prologue(void* prologue_address);
-void set_header(void* header_address, unsigned int allocated, unsigned int block_size, unsigned int padding);
 void set_footer(void* footer_address, unsigned int allocated, unsigned int block_size);
-
+void set_free_header(void* free_header_address, unsigned int block_size, ics_free_header* next, ics_free_header* prev);
+unsigned int get_size_from_block_size(unsigned int block_size);
+void set_allocated_block(void* block_address, unsigned int block_size, unsigned int padding);
+void set_free_block(void* block_address, unsigned int block_size, ics_free_header* next, ics_free_header* prev);
+void insert_head(void* block_address, unsigned int block_size);
 
 #endif
--- src/helpers.c
+++ src/helpers.c
@@ -3,24 +3,17 @@
 #include "icsmm.h"
 
 /* Helper function definitions go here */
-unsigned int calculate_ceiling(unsigned int a, unsigned int b)
+unsigned int ceiling(unsigned int a, unsigned int b)
 {
-    return (a / b) + ((a % b) != 0);
-}
-
-unsigned int get_total_payload(unsigned int requested_payload)
-{
-    return calculate_ceiling(requested_payload, PAYLOAD_UNIT);
+    int result = (a / b) + ((a % b) != 0);
+    return result;
 }
 
 void get_total_block_size(unsigned int requested_payload, unsigned int* block_size, unsigned int* padding)
 {
     *block_size = 0;
-
-    unsigned int total_payload = get_total_payload(requested_payload);
+    unsigned int total_payload = ceiling(requested_payload, PAYLOAD_UNIT) * PAYLOAD_UNIT;
     *padding = total_payload - requested_payload;
-
-    if(first_request) *block_size += PROLOGUE_SIZE + EPILOGUE_SIZE;
     *block_size += HEADER_SIZE + FOOTER_SIZE;
     *block_size += total_payload;
 }
@@ -55,4 +48,39 @@ void set_footer(void* footer_address, unsigned int allocated, unsigned int block
     footer->fid = FOOTER_MAGIC;
 }
 
-// TODO: add function to insert, which will call helper function set free header
+void set_free_header(void* free_header_address, unsigned int block_size,
+                     struct ics_free_header *next, struct ics_free_header *prev)
+{
+    ics_free_header* free_header = free_header_address;
+    set_header(free_header_address, NOT_ALLOCATED, block_size, 0);
+    free_header->next = next;
+    free_header->prev = prev;
+}
+
+unsigned int get_size_from_block_size(unsigned int block_size)
+{
+    return (block_size >> 1) << 1;
+}
+
+void set_allocated_block(void* block_address, unsigned int block_size, unsigned int padding)
+{
+    set_header(block_address, ALLOCATED, block_size, padding);
+    void* footer_address = block_address + block_size - FOOTER_SIZE;
+    set_footer(footer_address, ALLOCATED, block_size);
+}
+
+void set_free_block(void* block_address, unsigned int block_size,
+                    ics_free_header* next, ics_free_header* prev)
+{
+    set_free_header(block_address, block_size, next, prev);
+    void* footer_address = block_address + block_size - FOOTER_SIZE;
+    set_footer(footer_address, NOT_ALLOCATED, block_size);
+}
+
+void insert_head(void* block_address, unsigned int block_size)
+{
+    freelist_head->prev = block_address;
+    set_free_header(block_address, block_size, freelist_head, NULL);
+    freelist_head = block_address;
+}
+
